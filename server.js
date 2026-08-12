0d6998fe7e87abda06615e0fdbdcb11668603fbb
Akshita Sharma <akshitasharma@Akshitas-MacBook-Air.local>
2026-08-12 09:53:28 +0530

    Day 2: Authentication

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const pool = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("QuizPlatform Backend is Running 🚀");
+// Test PostgreSQL connection
+pool.query("SELECT NOW()", (err, result) => {
+  if (err) {
+    console.error("❌ Database connection failed:", err.message);
+  } else {
+    console.log("✅ PostgreSQL connected:", result.rows[0]);
+  }
 });
 
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("QuizPlatform Backend is Running 🚀");
+});
+
+// Start server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
